63ab0529c8416b18c3bba62d319e6eaa929b0617
Dilan Andrade <dilan.andrade@epn.edu.ec>
2024-07-08 22:01:54 -0500

    dilan/be/Conectar con la base de datos MongosDB

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-/*
+
 import authRoutes from "./routes/auth.routes.js";
-import taksRoutes from "./routes/tasks.routes.js";*/
+import taksRoutes from "./routes/tasks.routes.js";
 import { FRONTEND_URL } from "./config.js";
 
 const app = express();
@@ -18,9 +18,9 @@ app.use(
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
-/*
+
 app.use("/api/auth", authRoutes);
-app.use("/api", taksRoutes);*/
+app.use("/api", taksRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const path = await import("path");
